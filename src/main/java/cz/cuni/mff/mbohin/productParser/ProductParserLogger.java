9afb3d4c825f52348afde97cf52f3a9144daa87f
Michael Bohin <mbohin@seznam.cz>
2024-05-28 17:31:17 +0200

    implemented abstract generic class Adapter

--- src/main/java/cz/cuni/mff/mbohin/productParser/ProductParserLogger.java
+++ src/main/java/cz/cuni/mff/mbohin/productParser/ProductParserLogger.java
@@ -1,4 +1,33 @@
 package cz.cuni.mff.mbohin.productParser;
 
+import cz.cuni.mff.mbohin.productParser.normalizedJsonSchema.NormalizedProduct;
+import cz.cuni.mff.mbohin.productParser.normalizedJsonSchema.Eshop;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class ProductParserLogger {
+
+    private static final String LOGS_PATH = "./out/devLogs/";
+
+    private static String parserLogName(Eshop eshop) {
+        return "parsed" + eshop + "Products.txt";
+    }
+
+    public static void log(List<NormalizedProduct> products, Eshop eshop) {
+        Path logPath = Paths.get(LOGS_PATH + parserLogName(eshop));
+        try {
+            Files.createDirectories(logPath.getParent());  // Ensure directories are created
+            try (BufferedWriter writer = Files.newBufferedWriter(logPath)) {
+                for (NormalizedProduct product : products) {
+                    writer.write(product.toString() + "\n");
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to write to log file: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/cz/cuni/mff/mbohin/productParser/adapters/Adapter.java
+++ src/main/java/cz/cuni/mff/mbohin/productParser/adapters/Adapter.java
@@ -1,4 +1,100 @@
 package cz.cuni.mff.mbohin.productParser.adapters;
 
-public class Adapter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import cz.cuni.mff.mbohin.productParser.ProductParserLogger;
+import cz.cuni.mff.mbohin.productParser.normalizedJsonSchema.NormalizedProduct;
+import cz.cuni.mff.mbohin.productParser.normalizedJsonSchema.Eshop;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class Adapter<T> {
+
+    protected abstract boolean anyCriticalPropertyIsNull(T product);
+
+    protected abstract NormalizedProduct unsafeParseNormalizedProduct(T product);
+
+    protected abstract String getNameOf();
+
+    protected abstract String getRelativeDataPath();
+
+    protected abstract Eshop getEshopType();
+
+    public List<NormalizedProduct> getNormalizedProducts() throws IOException {
+        String json = FileHandler.loadJsonFromPath(getRelativeDataPath());
+        return parseNormalizedProducts(json);
+    }
+
+    public List<NormalizedProduct> getNormalizedProducts(String zipExtractPath) throws IOException {
+        String json = FileHandler.loadJsonFromPath(getRelativeDataPath(), zipExtractPath);
+        return parseNormalizedProducts(json);
+    }
+
+    private List<NormalizedProduct> parseNormalizedProducts(String json) throws IOException {
+        List<T> jsonProducts = deserializeProducts(json);
+        Pair<List<NormalizedProduct>, List<T>> productsPair = processProducts(jsonProducts);
+
+        logProductCounts(productsPair.getKey(), jsonProducts.size(), productsPair.getValue().size());
+        return productsPair.getKey();
+    }
+
+    private List<T> deserializeProducts(String json) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        TypeReference<List<T>> typeRef = new TypeReference<List<T>>() {};
+        return mapper.readValue(json, typeRef);
+    }
+
+    /*private Pair<List<NormalizedProduct>, List<T>> processProducts(List<T> products) {
+        List<NormalizedProduct> normalizedProducts = new ArrayList<>();
+        List<T> invalidProducts = new ArrayList<>();
+
+        for (T product : products) {
+            NormalizedProduct normalizedProduct;
+            if (tryGetNormalized(product, out normalizedProduct)) {
+                normalizedProducts.add(normalizedProduct);
+            } else {
+                invalidProducts.add(product);
+            }
+        }
+
+        return new Pair<>(normalizedProducts, invalidProducts);
+    }*/
+
+    private Pair<List<NormalizedProduct>, List<T>> processProducts(List<T> products) {
+        List<NormalizedProduct> normalizedProducts = new ArrayList<>();
+        List<T> invalidProducts = new ArrayList<>();
+
+        for (T product : products) {
+            Pair<Boolean, NormalizedProduct> result = tryGetNormalized(product);
+            if (result.getKey()) {
+                normalizedProducts.add(result.getValue());
+            } else {
+                invalidProducts.add(product);
+            }
+        }
+
+        return new Pair<>(normalizedProducts, invalidProducts);
+    }
+
+    private Pair<Boolean, NormalizedProduct> tryGetNormalized(T product) {
+        if (anyCriticalPropertyIsNull(product)) {
+            return new Pair<>(false, null);
+        } else {
+            NormalizedProduct normalizedProduct = unsafeParseNormalizedProduct(product);
+            return new Pair<>(true, normalizedProduct);
+        }
+    }
+
+    private void logProductCounts(List<NormalizedProduct> normalizedProducts, int total, int invalidCount) {
+        ProductParserLogger.log(normalizedProducts, getEshopType());
+
+        System.out.println(getNameOf());
+        System.out.println(total);
+        System.out.println("Normalized products: " + normalizedProducts.size());
+        System.out.println("Invalid products: " + invalidCount);
+        System.out.println(normalizedProducts.size() + " + " + invalidCount + " = " + total + "\n");
+    }
 }
